d8de5c3c783f55659095ee9d913dbdab107cc0f5
Emersumm <emersonbk15@gmail.com>
2025-02-15 14:30:24 -0300

    Code/rebbitConfig

--- BankAccount/src/main.ts
+++ BankAccount/src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    await app.connectMicroservice({
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:admin@localhost:5672'],
+          queue: 'default-nestjs-transaction',
+          noAck: false,
+          queueOptions: {
+            durable: true,
+          },
+        }});
 
     const config = new DocumentBuilder()
         .setTitle('BankAccount')
@@ -18,7 +28,7 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({ transform: true, whitelist: true }),
     );
-
+    await app.startAllMicroservices();
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
